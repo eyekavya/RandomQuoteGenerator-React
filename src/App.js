60cb6e9d6986964430842b8af46d239f2beefbee
eyekavya <kavyasrivastava7830@gmail.com>
2023-02-06 13:09:16 +0530

    added quotes component

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import "./App.css";
 import Navbar from "./components/Navbar";
+import Quotes from "./components/Quotes";
 
 function App() {
   return (
     <>
       <Navbar />
+      <Quotes />
     </>
   );
 }
---
+++ src/components/Quotes.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+function Quotes() {
+  return (
+    <>
+      <div className="container my-5"></div>
+    </>
+  );
+}
+
+export default Quotes;
