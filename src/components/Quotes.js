a4b82c810a3ea0189805f057fbf53d97dc6aa97a
eyekavya <shivam@Shivams-MacBook-Pro.local>
2023-02-18 17:00:46 +0530

    integrated with quotes api

--- src/components/Quotes.js
+++ src/components/Quotes.js
@@ -1,7 +1,27 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import QuotesItems from "./QuotesItems";
 
 function Quotes() {
+  const [articles, setArticles] = useState([]);
+
+  const updateQuotes = async () => {
+    await fetch("https://api.api-ninjas.com/v1/quotes?category=happiness", {
+      method: "GET",
+      headers: {
+        "Content-Type": "application/json",
+        "X-Api-Key": process.env.REACT_APP_QUOTES_API,
+      },
+    }).then((res) => {
+      res.text().then((txt) => {
+        console.log(JSON.parse(txt));
+      });
+    });
+  };
+
+  useEffect(() => {
+    updateQuotes();
+  }, []);
+
   return (
     <>
       <div className="container my-5">
