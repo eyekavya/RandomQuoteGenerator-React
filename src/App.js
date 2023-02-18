b6b8f925b4bbe474b2d51609248f19b3b3eff5e0
Shivam <shivam@Shivams-MacBook-Pro.local>
2023-02-18 16:09:20 +0530

    integrated with quotes api

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import Navbar from "./components/Navbar";
 import Quotes from "./components/Quotes";
 
 function App() {
+  console.log(process.env.REACT_APP_QUOTES_API);
   return (
     <>
       <Navbar />
